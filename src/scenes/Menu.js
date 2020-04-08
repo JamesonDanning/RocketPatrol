82bd8ac029b8038086edd7c1332eec5fa06359c6
Jameson Danning <jdanning@ucsc.edu>
2020-04-08 15:47:18 -0700

    scenes and assets implemented

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,5 +1,11 @@
 class Menu extends Phaser.Scene {
-    constructor() {
-        super("menuScene");
+    constructor(){
+        super("memuScene")
+    }
+
+    create() {
+        this.add.text(20, 20, "Rocket Patrol Menu");
+
+        this.scene.start("playScene");
     }
 }
